d5e9d7f6954bc8d7311a68906cd09fcc9152e162
Mohamed Shadhaan <shadhanm@gmail.com>
2025-08-07 22:17:03 +0800

    Initial Commit

---
+++ FairplayTests/FairplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  FairplayTests.swift
+//  FairplayTests
+//
+//  Created by Mohamed Shadhaan on 07/08/2025.
+//
+
+import Testing
+@testable import Fairplay
+
+struct FairplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
